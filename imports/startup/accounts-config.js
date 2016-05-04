75124bdfec51d257554b8ace2ecc8eafd28f378e
Tamim Wahage <twahage@tzi.de>
2016-05-04 18:19:46 +0200

    EMail oder Username bei der Anmeldung jetzt möglich

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -14,4 +14,25 @@ AccountsTemplates.configureRoute('signUp', {
   path: '/signup'
 });
 
+var pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+      _id: "username",
+      type: "text",
+      displayName: "username",
+      required: true,
+      minLength: 5,
+  },
+  {
+      _id: 'email',
+      type: 'email',
+      required: true,
+      displayName: "email",
+      re: /.+@(.+){2,}\.(.+){2,}/,
+      errStr: 'Invalid email',
+  },
+  pwd
+]);
+
 AccountsTemplates._init();
